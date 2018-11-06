fc4398956b58a1c68ea403a9b75acd87a878a52f
Misha Wagner <mishajw@gmail.com>
2018-11-06 20:06:14 +0000

    Add player to toggle other players on/off

--- src/lib/players/mod.rs
+++ src/lib/players/mod.rs
@@ -1,9 +1,11 @@
 //! Implementations of [`Player`](../core/trait.Player.html)
 
 mod combiner;
+mod toggle;
 mod volume;
 mod wave;
 
 pub use self::combiner::Combiner;
+pub use self::toggle::Toggle;
 pub use self::volume::Volume;
 pub use self::wave::Wave;
--- src/lib/spec/creation.rs
+++ src/lib/spec/creation.rs
@@ -25,6 +25,7 @@ pub fn create_player(spec: &mut Spec) -> Result<Box<Player>> {
     create_with_type::<players::Wave, _>(&name, spec)
         .or_else(|| create_with_type::<players::Volume, _>(&name, spec))
         .or_else(|| create_with_type::<players::Combiner, _>(&name, spec))
+        .or_else(|| create_with_type::<players::Toggle, _>(&name, spec))
         .unwrap_or_else(|| Err(ErrorKind::SpecUnknownName(name.clone()).into()))
         .chain_err(|| format!("Failed to create player {}", name))
 }
