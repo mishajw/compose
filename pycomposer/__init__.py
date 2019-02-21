12883a588c18ceabb3a240123f394b8de3565af4
Misha Wagner <mishajw@gmail.com>
2019-02-21 19:31:18 +0000

    Add freq-resolver calling to pycomposer

--- pycomposer/__init__.py
+++ pycomposer/__init__.py
@@ -2,6 +2,7 @@
 Library for generating specs for composer
 """
 
+from .frequency_resolver import chord, scale
 from .misc import Time
 from .to_spec import ToSpec
 from .gen import *
--- src/lib/core/spec/read/python.rs
+++ src/lib/core/spec/read/python.rs
@@ -2,12 +2,14 @@ use core::spec::read::yaml_string_to_spec;
 use core::spec::Spec;
 use error::*;
 
+use std::env;
 use std::process::Command;
 
 /// Execute a python file and return the spec
 pub fn python_string_to_spec(python_str: String) -> Result<Spec> {
     let output = Command::new("python")
         .args(&["-c", &python_str])
+        .env("PATH", &get_path()?)
         .output()
         .chain_err(|| "Failed to execute python file for spec")?;
     let stdout = String::from_utf8(output.stdout)
@@ -21,4 +23,17 @@ pub fn python_string_to_spec(python_str: String) -> Result<Spec> {
         )));
     }
     yaml_string_to_spec(stdout)
+}
+
+fn get_path() -> Result<String> {
+    let current_path = env::var("PATH").chain_err(|| "No PATH env variable")?;
+    let executable_path =
+        env::current_exe().chain_err(|| "Could not get executable path")?;
+    let executable_directory = executable_path
+        .parent()
+        .chain_err(|| "Could not get exetuable directory")?;
+    let executable_directory_str = executable_directory
+        .to_str()
+        .chain_err(|| "Could not get executable directory string")?;
+    Ok(format!("{}:{}", current_path, executable_directory_str))
 }
